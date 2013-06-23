636c4af3f9e44bf856092ff5e7e1a8635e385afc
Rajib Ahmed <l.rajibahmed@gmail.com>
2013-06-23 15:08:37 +0200

    Add composer task template

--- Capfile
+++ Capfile
@@ -1,4 +1,30 @@
 require 'rubygems'
 require 'railsless-deploy'
 
-load 'config/deploy' # remove this line to skip loading any of the default tasks
+load 'config/deploy' # remove this line to skip loading any of the default task
+
+namespace :asset do
+    desc "compress assets"
+    task :compress do
+
+    end
+end
+
+
+namespace :composer do
+    desc "copy vendor directory"
+    task :copy  do
+
+    end
+
+    desc "install php libraries"
+    task :install  do
+
+    end
+
+
+    desc "update composer"
+    task :update do
+
+    end
+end
--- config/deploy.rb
+++ config/deploy.rb
@@ -1,19 +1,19 @@
-set :application, "Silex-starterKit"
+set :application, "starterKit"
 set :repository,  "git@github.com:rajibahmed/silex-starterKit.git"
 set :branch,      ""
 
 set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 #
-set :sudo, false #for using deploy user not the root user
-set :deploy_to, "/var/www/#{application}" #installation path on server
-set :deploy_via, :remote_cache
+set :deploy_to, "/home/rajib/Sites/php/#{application}" #installation path on server
+set :deploy_via, :copy
 
-set :domain,      "" #domain where application would be installed
-set :user,        "" #set deploy user
+set :domain,    "" #domain where application would be installed
+set :user,      "rajib" #set deploy user
+set :sudo,      false #for using deploy user not the root user
 
 role :web,        domain
-role :app,        domain
+role :app,        ""
 role :db,         domain, :primary => true
 
 ssh_options[:forward_agent] = true
